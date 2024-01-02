099fffdb693cb0da7fdb0cc4ee3a57c9aac65276
krutarthpurohit2023 <krutarth.purohit@bacancy.com>
2024-01-02 09:13:56 +0530

    delete few lines

--- test1.c
+++ test1.c
@@ -3,12 +3,7 @@
 int main(){
 
     int a;
-    printf("Enter the data- ");
-    scanf("%d",&a);
-
-    printf("\n\n%d is the data",a);
-
-    printf("\n\ndata after adding 9 is- %d",a+9);
+    
 
 
     return 0;
